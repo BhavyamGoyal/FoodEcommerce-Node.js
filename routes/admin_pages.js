2c6bc5e31576b661c23bdf2fed737e19d668be93
shubhamsatbhaiya <300shubhamjain@gmail.com>
2020-02-01 01:05:23 +0530

    video 10 complete admin pages done

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var router = express.Router();
-
+//==============
+//Get pages Index
+//=============
 router.get('/',function(req,res){
     res.send('Admin Area');
     
 });
-router.get('/test',function(req,res){
-    res.send('Admin Area Test');
+//==============
+//Get add page
+//=============
+router.get('/add-page',function(req,res){
     
+    var title ="";
+    var slug ="";
+    var content ="";
+
+    res.render('admin/add-page',{
+        title: title,
+        slug:slug,
+        content:content
+    });
 });
 //Exports
 module.exports = router;
