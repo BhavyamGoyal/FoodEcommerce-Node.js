e878145fc4cc385396b29c90efc57fc02c39b13d
shubhamsatbhaiya <300shubhamjain@gmail.com>
2020-02-01 17:22:40 +0530

    video 12 complete/some error

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 //==============
+//Get page Model
+//=============
+var Page = require('../models/page');
+
+//==============
 //Get pages Index
 //=============
 router.get('/',function(req,res){
@@ -46,7 +51,33 @@ router.post('/add-page',function(req,res){
         });
     }
     else{
-        console.log('Success');
+        Page.findOne({slug :slug},function(err,page){
+            if(page){
+                req.flash('danger','Page slug exist, choose another.');
+                res.render('admin/add-page',{
+                    
+                    title: title,
+                    slug:slug,
+                    content:content
+                });
+            }else{
+                var page = new Page({
+                    title: title,
+                    slug:slug,
+                    content:content,
+                    sorting:0
+                });
+                page.save(function(err){
+                    if(err){
+                        return console.log(err);
+                    }
+                    req.flash('success','Page added!');
+                    req.redirect('/admin/pages');
+                });
+
+                
+            }
+        });
     }
     
 });
