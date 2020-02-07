5f134a436a8962f6d968e80bb65bfc3348ec900b
shubhamsatbhaiya <300shubhamjain@gmail.com>
2020-02-08 03:04:07 +0530

    video 36 complete/By clicking on navigation page we can see its description

--- routes/pages.js
+++ routes/pages.js
@@ -1,14 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
+//==============
+//Get page Model
+//=============
+var Page = require('../models/page');
+
+//Get
+
 router.get('/',function(req,res){
     //res.send('working on my project yo!!!');
-    res.render('index',{
-        title:'Home'
+    Page.findOne({slug: 'home'},function(err,page){
+        if(err){
+            console.log(err);
+        }
+       
+            res.render('index',{
+                title: page.title,
+                content: page.content
+            });
+
+       
     });
+    
 });
-router.get('/test',function(req,res){
-    res.send('Pages Test');
+
+//Get a Page
+router.get('/:slug',function(req,res){
+    var slug = req.params.slug;
+
+    Page.findOne({slug: slug},function(err,page){
+        if(err){
+            console.log(err);
+        }
+        if(!page){
+            res.redirect('/');
+        }else{
+            res.render('index',{
+                title: page.title,
+                content: page.content
+            });
+
+        }
+    });
     
 });
 //Exports
