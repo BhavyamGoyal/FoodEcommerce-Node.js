4575f6e37bbfe0449a5dfee493af73da48a39465
shubhamsatbhaiya <300shubhamjain@gmail.com>
2020-02-07 22:49:22 +0530

    video 34 complete /added pages are showing in the navigation tab

--- app.js
+++ app.js
@@ -26,6 +26,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Set Globalerror variable
 app.locals.errors = null;
 
+//Get page Model
+var Page = require("./models/page");
+
+//Get all pages to pass to header.ejs
+Page.find({}).sort({sorting:1}).exec(function (err,pages){
+  if(err){
+    console.log(err);
+  }
+  else{
+    app.locals.pages = pages;
+  }
+});
+
 //Express fileupload middleware
 app.use(fileUpload());
 
